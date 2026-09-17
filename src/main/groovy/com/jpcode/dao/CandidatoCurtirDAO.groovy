6dcfe35374c90cc67563f8a21b25101a4871615a
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-17 16:56:56 -0300

    feat: Iniciando CandidatoCurtirDAO

--- src/main/groovy/com/jpcode/dao/CandidatoCurtirDAO.groovy
+++ src/main/groovy/com/jpcode/dao/CandidatoCurtirDAO.groovy
@@ -1,4 +1,27 @@
 package com.jpcode.dao
 
+import com.jpcode.database.ConnectionFactory
+import com.jpcode.model.Candidato
+import com.jpcode.model.Vaga
+
+import java.sql.Statement
+
 class CandidatoCurtirDAO {
+    void salvar(Candidato candidato, Vaga vaga) {
+        String sql = """
+            INSERT INTO vagas_curtidas_candidato
+                (id_candidato, id_vaga)
+            VALUES (?, ?)
+        """
+
+        try (
+            def connection = ConnectionFactory.getConnection(sql)
+            def statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            statement.setLong(1, candidato.id)
+            statement.setLong(2, vaga.id)
+
+            statement.executeUpdate()
+        }
+    }
 }
