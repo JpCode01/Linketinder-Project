ae0900db6765c499c87a26554e8689ece3358d30
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-17 18:31:03 -0300

    feat: Adicionando metodo para buscar vaga em CompetenciasCandidatoDAO

--- src/main/groovy/com/jpcode/dao/candidato/CompetenciasCandidatoDAO.groovy
+++ src/main/groovy/com/jpcode/dao/candidato/CompetenciasCandidatoDAO.groovy
@@ -15,14 +15,46 @@ class CompetenciasCandidatoDAO {
         """
 
         try (
-            def connection = ConnectionFactory.getConnection(sql)
-            def statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+            def connection = ConnectionFactory.getConnection()
+            def statement = connection.prepareStatement(sql)
         ) {
             statement.setLong(1, candidato.id)
             statement.setLong(2, competencia.id)
 
             statement.executeUpdate()
         }
-
     }
+
+    List<Competencia> buscarPorVaga(Long idVaga) {
+        String sql = """
+            SELECT c.id, c.nome_competencia
+            FROM competencias c
+            JOIN candidatos_competencias cc
+            ON cc.id_competencia = c.id
+            WHERE cc.id_vaga = ?
+        """
+
+        try (
+            def connection = ConnectionFactory.getConnection(sql)
+            def statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            statement.setLong(1, idVaga)
+
+            def resultSet = statement.executeQuery()
+
+            List<Competencia> competencias = []
+
+            while (resultSet.next()) {
+                competencias.add(
+                        new Competencia(
+                                resultSet.getLong("id"),
+                                resultSet.getString("nome_competencia")
+                        )
+                )
+            }
+
+            return competencias
+        }
+    }
+
 }
