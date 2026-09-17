fe0cb6758be333861d1f11a4d19dde2c68a5a3cb
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-17 12:04:23 -0300

    feat: Atualizando models e corrigindo para novos campos do BD

--- src/main/groovy/com/jpcode/model/Candidato.groovy
+++ src/main/groovy/com/jpcode/model/Candidato.groovy
@@ -1,20 +1,28 @@
 package com.jpcode.model
 
+import java.time.LocalDate
+
 class Candidato extends Pessoa {
     String cpf
     int idade
+    String sobrenome
+    LocalDate dataNascimento
     List<Vaga> vagasCurtidas = []
 
-    Candidato(String nome, String email, String cpf, Integer idPais, int idade, String estado, String cep, String descricao) {
+    Candidato(String nome, String sobrenome, String email, String cpf, LocalDate dataNascimento,Integer idPais, int idade, String estado, String cep, String descricao) {
         super(nome, email, estado, cep, descricao, idPais)
         this.cpf = cpf
         this.idade = idade
+        this.sobrenome = sobrenome
+        this.dataNascimento = dataNascimento
     }
 
-    Candidato(Long id, String nome, String email, String cpf, Integer idPais, int idade, String estado, String cep, String descricao) {
+    Candidato(Long id, String nome, String sobrenome, String email, String cpf, LocalDate dataNascimento, Integer idPais, int idade, String estado, String cep, String descricao) {
         super(nome, email, estado, cep, descricao, idPais, id)
         this.cpf = cpf
         this.idade = idade
+        this.sobrenome = sobrenome
+        this.dataNascimento = dataNascimento
     }
 
     @Override
--- src/main/groovy/com/jpcode/model/Pessoa.groovy
+++ src/main/groovy/com/jpcode/model/Pessoa.groovy
@@ -11,17 +11,21 @@ class Pessoa implements PessoaInterface {
     String descricao
     Long idPais
     List competencias = []
+    String senha
+    boolean ativo
 
-    Pessoa(String nome, String email, String estado, String cep, String descricao, Long idPais) {
+    Pessoa(String nome, String email, String senha, String estado, String cep, String descricao, Long idPais) {
         this.nome = nome
         this.email = email
         this.estado = estado
         this.cep = cep
         this.descricao = descricao
         this.idPais = idPais
+        this.ativo = true
+        this.senha = senha
     }
     
-    Pessoa(String nome, String email, String estado, String cep, String descricao,  Long idPais, Long id) {
+    Pessoa(String nome, String email, String senha, String estado, String cep, String descricao,  Long idPais, Long id) {
         this.nome = nome
         this.email = email
         this.estado = estado
@@ -29,10 +33,17 @@ class Pessoa implements PessoaInterface {
         this.descricao = descricao
         this.idPais = idPais
         this.id = id
+        this.senha = senha
     }
 
     String adicionarCompetencia(CompetenciasEnum competencia) {
         competencias.add(competencia)
         competencias.last()
+    }
+
+    void desativarUser() {
+        if (this.ativo == true) {
+            this.ativo = false
+        }
     }
 }
