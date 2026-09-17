4427af168651c5cb3421266254ca3742e51cf523
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-17 14:33:12 -0300

    feat: Iniciando PaisDAO

--- src/main/groovy/com/jpcode/dao/PaisDAO.groovy
+++ src/main/groovy/com/jpcode/dao/PaisDAO.groovy
@@ -1,4 +1,34 @@
 package com.jpcode.dao
 
+import com.jpcode.database.ConnectionFactory
+import com.jpcode.model.Pais
+
 class PaisDAO {
+
+    Pais buscarPorId(Long id) {
+        String sql = """
+            SELECT id, nome
+            FROM pais
+            WHERE id = ?
+        """
+
+        try (
+            def connection = ConnectionFactory.getConnection(sql)
+            def statement = connection.prepareStatement(sql)
+        ) {
+            statement.setLong(1, id)
+
+            def resultSet = statement.executeQuery()
+
+            if (!resultSet.next()) {
+                return null
+            }
+
+            return new Pais(
+                    resultSet.getLong("id"),
+                    resultSet.getString("nome")
+                    
+            )
+        }
+    }
 }
