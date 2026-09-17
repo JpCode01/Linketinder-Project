9fa230fc04176b184613e62262924ccea6d54d3e
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-17 18:21:52 -0300

    feat: Adicionando metodo para buscar vaga em CompetenciasVagaDAO

--- src/main/groovy/com/jpcode/dao/vaga/CompetenciasVagaDAO.groovy
+++ src/main/groovy/com/jpcode/dao/vaga/CompetenciasVagaDAO.groovy
@@ -24,4 +24,35 @@ class CompetenciasVagaDAO {
             statement.executeUpdate()
         }
     }
+
+    List<Competencia> buscarPorVaga(Long idVaga) {
+        String sql = """
+            SELECT c.id, c.nome_competencia
+            FROM competencias c
+            JOIN vagas_competencias vc
+            ON vc.id_competencia = c.id
+            WHERE vc.id_vaga = ?
+        """
+
+        try (
+            def connection = ConnectionFactory.getConnection()
+            def statement = connection.prepareStatement(sql)
+        ) {
+            statement.setLong(1, idVaga)
+
+            def resultSet = statement.executeQuery()
+
+            List<Competencia> competencias = []
+
+            while (resultSet.next()) {
+                competencias.add(
+                        new Competencia(
+                                resultSet.getLong("id"),
+                                resultSet.getString("nome_competencia")
+                        )
+                )
+            }
+            return competencias
+        }
+    }
 }
