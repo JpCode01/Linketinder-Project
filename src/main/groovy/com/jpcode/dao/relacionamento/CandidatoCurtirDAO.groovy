d9f42f8c13b5fe32af892c7d9bcaa1d18fcaf05a
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-17 19:55:07 -0300

    feat: Implementando metodo para buscar vagas curtida por id do candidato

--- src/main/groovy/com/jpcode/dao/relacionamento/CandidatoCurtirDAO.groovy
+++ src/main/groovy/com/jpcode/dao/relacionamento/CandidatoCurtirDAO.groovy
@@ -24,4 +24,41 @@ class CandidatoCurtirDAO {
             statement.executeUpdate()
         }
     }
+
+    List<Vaga> buscarVagasCurtidas(Long idCandidato) {
+        String sql = """
+            SELECT v.id, v.nome, v.descricao,
+                v.local
+            FROM vagas v
+            JOIN vagas_curtidas_candidato vcc
+            ON vcc.id_vaga = v.id
+            WHERE vcc.id_candidato = ? 
+        """
+
+        try (
+            def connection = ConnectionFactory.getConnection()
+            def statement = connection.prepareStatement(sql)
+        ) {
+            statement.setLong(1, idCandidato)
+
+            def resultSet = statement.executeQuery()
+
+            List<Vaga> vagas = []
+
+            while (resultSet.next()) {
+                vagas.add(
+                        new Vaga(
+                                resultSet.getLong("id"),
+                                resultSet.getString("nome"),
+                                resultSet.getString("descricao"),
+                                resultSet.getString("local"),
+                                null
+                        )
+                )
+            }
+
+            return vagas
+        }
+    }
+    
 }
