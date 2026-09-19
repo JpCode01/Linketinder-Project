7f28fdbbb687cb1aaf97bb481f39c768bbb04fd1
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-19 12:12:53 -0300

    feat: Iniciando EmpresaMatchDTO

--- src/main/groovy/com/jpcode/dto/empresa/EmpresaMatchDTO.groovy
+++ src/main/groovy/com/jpcode/dto/empresa/EmpresaMatchDTO.groovy
@@ -1,4 +1,42 @@
 package com.jpcode.dto.empresa
 
 class EmpresaMatchDTO {
+    Long id
+    String nome
+    String cnpj
+    String emailCorporativo
+    String descricao
+    String pais
+    String cep
+    String estado
+
+    EmpresaMatchDTO(Long id, String nome, String cnpj, String emailCorporativo, String descricao, String pais, String cep, String estado) {
+        this.id = id
+        this.nome = nome
+        this.cnpj = cnpj
+        this.emailCorporativo = emailCorporativo
+        this.descricao = descricao
+        this.pais = pais
+        this.cep = cep
+        this.estado = estado
+    }
+
+
+    @Override
+    public String toString() {
+        return """
+            ----------------------------------------------------------------
+
+            EMPRESA:
+            
+            ID: ${id}
+            NOME DA EMPRESA: ${nome} 
+            EMAIL CORPORATIVO: ${emailCorporativo}
+            DESCRICAO: ${descricao}
+            CNPJ: ${cnpj}
+            CEP: ${cep}
+            ESTADO: ${estado}
+            PAIS: ${pais}
+        """
+    }
 }
