3c361c14aaf616e04f258274a8d4b118b9827742
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-19 14:08:47 -0300

    feat: iniciando VagaEmpresaDTO

--- src/main/groovy/com/jpcode/dto/vaga/VagaEmpresaDTO.groovy
+++ src/main/groovy/com/jpcode/dto/vaga/VagaEmpresaDTO.groovy
@@ -1,5 +1,37 @@
 package com.jpcode.dto.vaga
 
+import com.jpcode.model.referencia.Competencia
+
 class VagaEmpresaDTO {
-    
+    Long id
+    String nome
+    String descricao
+    String local
+    Integer qtdeCandidatosQueCurtiram
+    List<Competencia> competencias
+
+    VagaEmpresaDTO(Long id, String nome, String descricao, String local, Integer qtdeCandidatosQueCurtiram) {
+        this.id = id
+        this.nome = nome
+        this.descricao = descricao
+        this.local = local
+        this.qtdeCandidatosQueCurtiram = qtdeCandidatosQueCurtiram
+    }
+
+
+    @Override
+    public String toString() {
+        return """
+        ---------------------------------
+
+        VAGA:
+        
+        ID: ${id}
+        NOME DA VAGA: ${nome}
+        DESCRIÇÃO: ${descricao}
+        LOCAL: ${local} 
+        QUANTIDADE DE CANDIDATOS QUE CURTIRAM: ${qtdeCandidatosQueCurtiram}
+        COMPETENCIAS: ${competencias.forEach {competencia -> print(competencia.nome + ", ")}}
+        """
+    }
 }
