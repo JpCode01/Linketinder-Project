ec1cd7112df78f94e8f1907549a0071018f84459
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-19 18:12:26 -0300

    feat: Atualizando metodo de buscarCandidatosQueCurtiram em CandidatoCurtirDAO para retornar Lista de CandidatoAnonimoDto

--- src/main/groovy/com/jpcode/dao/relacionamento/CandidatoCurtirDAO.groovy
+++ src/main/groovy/com/jpcode/dao/relacionamento/CandidatoCurtirDAO.groovy
@@ -1,8 +1,10 @@
 package com.jpcode.dao.relacionamento
 
 import com.jpcode.database.ConnectionFactory
+import com.jpcode.dto.candidato.CandidatoAnonimoDTO
 import com.jpcode.model.core.Candidato
 import com.jpcode.model.core.Vaga
+import com.jpcode.model.referencia.Competencia
 
 import java.sql.Statement
 
@@ -61,47 +63,74 @@ class CandidatoCurtirDAO {
         }
     }
 
-    List<Candidato> buscarCandidatosQueCurtiram(Long idVaga) {
+    List<CandidatoAnonimoDTO> buscarCandidatosQueCurtiram(Long idVaga) {
         String sql = """
-        SELECT c.id, c.descricao, c.ativo 
+        SELECT
+            c.id,
+            c.descricao,
+            comp.id AS competencia_id,
+            comp.nome_competencia
         FROM candidatos c
         JOIN vagas_curtidas_candidato vcc
-        ON vcc.id_candidato = c.id
-        WHERE vcc.id_vaga = ?        
+            ON vcc.id_candidato = c.id
+        LEFT JOIN candidatos_competencias cc
+            ON cc.id_candidato = c.id
+        LEFT JOIN competencias comp
+            ON comp.id = cc.id_competencia
+        WHERE vcc.id_vaga = ?
+          AND c.ativo = true
+        ORDER BY c.id
         """
 
         try (
-                def connection = ConnectionFactory.getConnection()
-                def statement = connection.prepareStatement(sql)
+            def connection = ConnectionFactory.getConnection()
+            def statement = connection.prepareStatement(sql)
         ) {
             statement.setLong(1, idVaga)
 
             def resultSet = statement.executeQuery()
 
-            List<Candidato> candidatos = []
+            List<CandidatoAnonimoDTO> candidatos = []
+
+            CandidatoAnonimoDTO candidatoAtual = null
+            List<Competencia> competencias = []
 
             while (resultSet.next()) {
-                if (resultSet.getBoolean("ativo")) {
-                    candidatos.add(
-                            new Candidato(
-                                    resultSet.getLong("id"),
-                                    "Candidato",
-                                    "Anônimo",
-                                    null,
-                                    null,
-                                    null,
-                                    null,
-                                    null,
-                                    -1,
-                                    null,
-                                    null,
-                                    resultSet.getString("descricao"),
-                                    resultSet.getBoolean("ativo")
-                            )
-                    )
-                }
+                Long idCandidato = resultSet.getLong("id")
 
+                if (candidatoAtual == null || candidatoAtual.id != idCandidato) {
+
+                    if (candidatoAtual != null) {
+                        candidatos.add(candidatoAtual)
+                    }
+
+                    competencias = []
+
+                    candidatoAtual = new CandidatoAnonimoDTO(
+                            idCandidato,
+                            resultSet.getString("descricao"),
+                            competencias
+                    )
+
+                    Long competenciaId = resultSet.getLong("competencia_id")
+
+                    if (competenciaId != 0) {
+                        competencias.add(
+                                new Competencia(
+                                        competenciaId,
+                                        resultSet.getString("nome_competencia")
+                                )
+                        )
+                    }
+                }
             }
+
+            if (candidatoAtual != null) {
+                candidatos.add(candidatoAtual)
+            }
+
+            return candidatos
+
         }
     }
 }
