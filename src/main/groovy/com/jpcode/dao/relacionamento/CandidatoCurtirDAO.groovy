f1949f206b0586f31463beca59b201afd7c7b296
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-20 16:23:10 -0300

    feat: Corrigindo metodo buscarVagasCurtidas em CandidatoCurtirDAO para retornar lista de VagaAnonimaDTO, e realizar consulta corretamente

--- src/main/groovy/com/jpcode/dao/relacionamento/CandidatoCurtirDAO.groovy
+++ src/main/groovy/com/jpcode/dao/relacionamento/CandidatoCurtirDAO.groovy
@@ -2,6 +2,7 @@ package com.jpcode.dao.relacionamento
 
 import com.jpcode.database.ConnectionFactory
 import com.jpcode.dto.candidato.CandidatoAnonimoDTO
+import com.jpcode.dto.vaga.VagaAnonimaDTO
 import com.jpcode.model.core.Candidato
 import com.jpcode.model.core.Vaga
 import com.jpcode.model.referencia.Competencia
@@ -27,36 +28,69 @@ class CandidatoCurtirDAO {
         }
     }
 
-    List<Vaga> buscarVagasCurtidas(Long idCandidato) {
+    List<VagaAnonimaDTO> buscarVagasCurtidas(Long idCandidato) {
         String sql = """
-            SELECT v.id, v.nome, v.descricao,
-                v.local
-            FROM vagas v
-            JOIN vagas_curtidas_candidato vcc
+        SELECT
+            v.id,
+            v.descricao,
+            comp.id AS competencia_id,
+            comp.nome_competencia
+        FROM vagas v
+        JOIN vagas_curtidas_candidato vcc
             ON vcc.id_vaga = v.id
-            WHERE vcc.id_candidato = ? 
+        LEFT JOIN vagas_competencias vc
+            ON vc.id_vaga = v.id
+        LEFT JOIN competencias comp
+            ON comp.id = vc.id_competencia
+        WHERE vcc.id_candidato = ?
+        ORDER BY v.id
         """
 
         try (
-            def connection = ConnectionFactory.getConnection()
-            def statement = connection.prepareStatement(sql)
+                def connection = ConnectionFactory.getConnection()
+                def statement = connection.prepareStatement(sql)
         ) {
             statement.setLong(1, idCandidato)
 
             def resultSet = statement.executeQuery()
 
-            List<Vaga> vagas = []
+            List<VagaAnonimaDTO> vagas = []
+
+            VagaAnonimaDTO vagaAtual = null
+            List<Competencia> competencias = []
 
             while (resultSet.next()) {
-                vagas.add(
-                        new Vaga(
-                                resultSet.getLong("id"),
-                                resultSet.getString("nome"),
-                                resultSet.getString("descricao"),
-                                resultSet.getString("local"),
-                                null
-                        )
-                )
+                Long idVaga = resultSet.getLong("id")
+
+                if (vagaAtual == null || vagaAtual.id != idVaga) {
+
+                    if (vagaAtual != null) {
+                        vagas.add(vagaAtual)
+                    }
+
+                    competencias = []
+
+                    vagaAtual = new VagaAnonimaDTO(
+                            idVaga,
+                            resultSet.getString("descricao"),
+                            competencias
+                    )
+                }
+
+                Long competenciaId = resultSet.getLong("competencia_id")
+
+                if (competenciaId != 0) {
+                    competencias.add(
+                            new Competencia(
+                                    competenciaId,
+                                    resultSet.getString("nome_competencia")
+                            )
+                    )
+                }
+            }
+
+            if (vagaAtual != null) {
+                vagas.add(vagaAtual)
             }
 
             return vagas
