8d59272d308c21da1d2bcbbb5a35a27095163922
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-20 08:43:13 -0300

    feat: Corrigindo metodo de cadastrarCandidato em CandidatoService

--- src/main/groovy/com/jpcode/service/CandidatoService.groovy
+++ src/main/groovy/com/jpcode/service/CandidatoService.groovy
@@ -1,38 +1,70 @@
 package com.jpcode.service
 
-import com.jpcode.enums.CompetenciasEnum
+import com.jpcode.dao.candidato.CandidatoDAO
+import com.jpcode.dao.candidato.CompetenciasCandidatoDAO
+import com.jpcode.dao.referencia.CompetenciaDAO
+import com.jpcode.dao.referencia.EstadoDAO
+import com.jpcode.dao.referencia.PaisDAO
 import com.jpcode.model.core.Candidato
 import com.jpcode.validation.CompetenciaValidation
 
+import java.time.LocalDate
+
 class CandidatoService {
     final CompetenciaValidation validation
+    final PaisDAO paisDAO
+    final EstadoDAO estadoDAO
+    final CompetenciaDAO competenciaDAO
+    final CompetenciasCandidatoDAO competenciasCandidatoDAO
 
-    CandidatoService(CompetenciaValidation validation) {
+    CandidatoService(CompetenciaValidation validation, PaisDAO paisDAO, EstadoDAO estadoDAO, CompetenciaDAO competenciaDAO, CompetenciasCandidatoDAO competenciasCandidatoDAO) {
         this.validation = validation
+        this.paisDAO = paisDAO
+        this.estadoDAO = estadoDAO
+        this.competenciaDAO = competenciaDAO
+        this.competenciasCandidatoDAO = competenciasCandidatoDAO
     }
     
     Candidato cadastrarCandidato(String nome, 
+                                 String sobrenome,
                                  String email,
+                                 String senha,
                                  String cpf,
+                                 LocalDate dataNascimento,
+                                 String pais,
                                  int idade,
                                  String estado,
                                  String cep,
                                  String descricao,
                                  List<String> competencias) {
-        Candidato candidato = new Candidato(nome, email, cpf, idade, estado, cep, descricao)
+        Long paisId = paisDAO.buscarIdPorNome(pais)
+        Long estadoId = estadoDAO.buscarIdPorSigla(estado)
+        Candidato candidato = new Candidato(
+                nome,
+                sobrenome,
+                email,
+                senha,
+                cpf,
+                dataNascimento,
+                paisId,
+                idade,
+                estadoId,
+                cep,
+                descricao
 
-        competencias.each { competencia ->
-            String competenciaNormalizada = competencia.toUpperCase()
+        )
 
-            if (validation.validarCompetencia(
-                    competenciaNormalizada,
-                    candidato.competencias
-            )) {
-                candidato.adicionarCompetencia(
-                        CompetenciasEnum.valueOf(competenciaNormalizada)
-                )
-            }
+        CandidatoDAO candidatoDAO = new CandidatoDAO()
+        Candidato candidatoSalvo = candidatoDAO.salvar(candidato)
+        
+        competencias.each {
+            competencia ->
+                Long idCompetenciaNormalizada = competenciaDAO.buscarIdPorNomeCompetencia(competencia)
+                if (idCompetenciaNormalizada != null) {
+                    competenciasCandidatoDAO.salvar(candidatoSalvo.id, idCompetenciaNormalizada)
+                }
         }
-        return candidato
+
+        return candidatoSalvo
     }
 }
